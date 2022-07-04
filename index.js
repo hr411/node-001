766f7b5bb42d0b53f90bce4b4fa190e0cc8df11f
Hyorin Song <hrsong411@gmail.com>
2022-07-04 22:25:44 +0900

    커밋

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan'); //npm install
 const app = express();
 
 //미들웨어
@@ -6,7 +7,27 @@ function logger(req, res, next){
     console.log('i am logger');
     next();
 }
-app.use(logger);
+
+function logger2(req, res, next){
+    console.log('i am logger2');
+    next();
+}
+
+function commonmw(req, res, next){
+    console.log('commonmw');
+    next(new Error('error ouccered'));
+}
+
+function errormw(err, req, res, next){
+    console.log(err.message);
+
+    next();
+}
+
+app.use(commonmw);
+app.use(errormw);
+app.use(morgan('dev')); 
+
 
 app.listen(3000, function(){
     console.log('Server is running');
