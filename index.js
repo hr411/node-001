ba5b07147626cae13d7103d5959145ffb226d0b7
Hyorin Song <hrsong411@gmail.com>
2022-07-04 22:28:55 +0900

    커밋

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const express = require('express');
-const morgan = require('morgan'); //npm install
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-//미들웨어
-function logger(req, res, next){
-    console.log('i am logger');
-    next();
-}
-
-function logger2(req, res, next){
-    console.log('i am logger2');
-    next();
-}
-
-function commonmw(req, res, next){
-    console.log('commonmw');
-    next(new Error('error ouccered'));
-}
-
-function errormw(err, req, res, next){
-    console.log(err.message);
-
-    next();
-}
-
-app.use(commonmw);
-app.use(errormw);
-app.use(morgan('dev')); 
-
-
-app.listen(3000, function(){
-    console.log('Server is running');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
- 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
