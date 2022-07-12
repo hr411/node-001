c257dfdb02c4b322a559e93a8602db083005a3de
Hyorin Song <hrsong411@gmail.com>
2022-07-12 23:57:47 +0900

    커밋

--- index.spec.js
+++ index.spec.js
@@ -117,4 +117,48 @@ describe('POST /users는 ', ()=> {
                 .end(done)
         })
     })
+});
+
+describe('PUT users/1는', ()=> {
+    describe('성공시', ()=>{
+        it('변경된 name을 응답한다', (done)=> {
+            const name = 'chally'
+            request(app)
+                .put('/users/3')
+                .send({name})
+                .end((err, res)=>{
+                    res.body.should.have.property('name',name);
+                    done();
+                });
+        })
+    })
+    describe('실패시', ()=>{
+        it('정수가 아닌 id일 경우 400을 응답한다',(done)=>{
+            request(app)
+                .put('/users/one')
+                .expect(400)
+                .end(done);
+        })
+        it('name이 없을 경우 400을 응답한다',(done)=>{
+            request(app)
+                .put('/users/1')
+                .send({})
+                .expect(400)
+                .end(done);
+        })
+        it('없는 유저일 경우 404을 응답한다',(done)=>{
+            request(app)
+                .put('/users/999')
+                .send({name: 'foo'})
+                .expect(404)
+                .end(done);
+        })
+        it('이름이 중복일 경우 409을 응답한다',(done)=>{
+            request(app)
+                .put('/users/3')
+                .send({name: 'bek'})
+                .expect(409)
+                .end(done);
+        })
+    })
 })
